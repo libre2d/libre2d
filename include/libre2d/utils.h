bc652c458c06cc5aefccacacf6de5731e10093e9
Paul Elder <paul.elder@libre2d.org>
2021-04-10 12:47:17 +0900

    utils: add utility functions for opengl shaders
    Signed-off-by: Paul Elder <paul.elder@libre2d.org>

---
+++ include/libre2d/utils.h
@@ -0,0 +1,34 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+/*
+ * Copyright (C) 2021, Paul Elder
+ *
+ * utils.h - Utility functions
+ */
+
+#ifndef __LIBRE2D_UTILS_H__
+#define __LIBRE2D_UTILS_H__
+
+#include <string>
+
+namespace libre2d {
+
+namespace utils {
+
+std::string loadFileToString(const char *path);
+
+namespace gl {
+
+bool compileShader(uint32_t shaderID, const std::string &source);
+
+uint32_t loadShadersFromStrings(const char *vertSource, uint32_t vertShaderID,
+				const char *fragSource, uint32_t fragShaderID);
+
+uint32_t loadShaders(const char *vertexFilePath, const char *fragmentFilePath);
+
+} /* namespace gl */
+
+} /* namespace utils */
+
+} /* namespace libre2d */
+
+#endif /* __LIBRE2D_MODEL_H__ */
---
+++ src/libre2d/utils.cpp
@@ -0,0 +1,127 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+/*
+ * Copyright (C) 2021, Paul Elder
+ *
+ * utils.cpp - Utility functions
+ */
+
+#include <GL/glew.h>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+namespace libre2d {
+
+namespace utils {
+
+std::string loadFileToString(const char *path)
+{
+	std::stringstream sstr;
+
+	std::ifstream file(path);
+	if (!file.is_open()) {
+		std::cerr << "Failed to open file " << path << std::endl;
+		return "";
+	}
+
+	sstr << file.rdbuf();
+	file.close();
+
+	return sstr.str();
+}
+
+namespace gl {
+
+bool compileShader(uint32_t shaderID, const std::string &source)
+{
+	GLint ret = GL_FALSE;
+	int infoLength;
+
+	const char *sourceStr = source.c_str();
+	glShaderSource(shaderID, 1, &sourceStr, NULL);
+	glCompileShader(shaderID);
+
+	glGetShaderiv(shaderID, GL_COMPILE_STATUS, &ret);
+	glGetShaderiv(shaderID, GL_INFO_LOG_LENGTH, &infoLength);
+	if (ret == GL_FALSE && infoLength > 0) {
+		std::vector<char> errMsg(infoLength + 1);
+		glGetShaderInfoLog(shaderID, infoLength, NULL, errMsg.data());
+		std::string errStr(errMsg.begin(), errMsg.end());
+		std::cerr << "Failed to compile shader: " << errStr << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+uint32_t loadShadersFromStrings(const char *vertSource, uint32_t vertShaderID,
+				const char *fragSource, uint32_t fragShaderID)
+{
+	GLuint vertexShaderID = vertShaderID ? vertShaderID :
+					       glCreateShader(GL_VERTEX_SHADER);
+	GLuint fragmentShaderID = fragShaderID ? fragShaderID :
+						 glCreateShader(GL_FRAGMENT_SHADER);
+
+	if (!compileShader(vertexShaderID, vertSource))
+		return 0;
+
+	if (!compileShader(fragmentShaderID, fragSource))
+		return 0;
+
+	GLuint programID = glCreateProgram();
+	glAttachShader(programID, vertexShaderID);
+	glAttachShader(programID, fragmentShaderID);
+	glLinkProgram(programID);
+
+	GLint ret = GL_FALSE;
+	int infoLength;
+	glGetProgramiv(programID, GL_LINK_STATUS, &ret);
+	glGetProgramiv(programID, GL_INFO_LOG_LENGTH, &infoLength);
+	if (ret == GL_FALSE && infoLength > 0) {
+		std::vector<char> errMsg(infoLength + 1);
+		glGetProgramInfoLog(programID, infoLength, NULL, errMsg.data());
+		std::string errStr(errMsg.begin(), errMsg.end());
+		std::cerr << "Failed to link program: " << errStr << std::endl;
+		return 0;
+	}
+
+	glDetachShader(programID, vertexShaderID);
+	glDetachShader(programID, fragmentShaderID);
+
+	if (!vertShaderID)
+		glDeleteShader(vertexShaderID);
+	if (!fragShaderID)
+		glDeleteShader(fragmentShaderID);
+
+	return programID;
+}
+
+uint32_t loadShaders(const char *vertexFilePath, const char *fragmentFilePath)
+{
+	GLuint vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+	std::string vertSource = loadFileToString(vertexFilePath);
+	if (vertSource.empty())
+		return 0;
+
+	std::string fragSource = loadFileToString(fragmentFilePath);
+	if (fragSource.empty())
+		return 0;
+
+	GLuint ret = loadShadersFromStrings(vertSource.c_str(), vertexShaderID,
+					    fragSource.c_str(), fragmentShaderID);
+	glDeleteShader(vertexShaderID);
+	glDeleteShader(fragmentShaderID);
+
+	return ret;
+}
+
+} /* namespace gl */
+
+} /* namespace utils */
+
+} /* namespace libre2d */
