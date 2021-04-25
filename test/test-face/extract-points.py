ec2e1fea22c1300416214ee1185fead138eddf02
Paul Elder <paul.elder@libre2d.org>
2021-04-25 15:00:32 +0900

    test: extract-points.py: Allow choosing colors
    To facilitate extracting points from a texture image for different
    Components, add an option for specifying the point color.
    
    Signed-off-by: Paul Elder <paul.elder@libre2d.org>

--- test/test-face/extract-points.py
+++ test/test-face/extract-points.py
@@ -4,22 +4,32 @@ from PIL import Image
 import sys
 
 if len(sys.argv) < 3:
-    print(f'Usage: {sys.argv[0]} <mode> <image>')
-    print(f'  Extract green (#00ff00) points from image')
+    print(f'Usage: {sys.argv[0]} <mode> <image> [color]')
+    print(f'  Extract color (default #00ff00) points from image')
     print(f'  mode: gl|img')
     print(f'        - gl:  upper left corner is [-1, 1], y grows up, x grows right')
     print(f'        - img: upper left corner is [ 0, 0], y grows down, x grows right')
+    print(f'  color: default 00ff00')
     exit()
 
 mode = sys.argv[1]
 im = Image.open(sys.argv[2])
+
+if len(sys.argv) >= 4:
+    c = int(sys.argv[3], 16)
+    r = (c & 0xff0000) >> 16
+    g = (c & 0xff00) >> 8
+    b = c & 0xff
+    color = (r, g, b, 255)
+else:
+    color = (0, 255, 0, 255)
 
 pixels = im.load()
 width, height = im.size
 
 for y in range(0, height):
     for x in range(0, width):
-        if pixels[x, y] == (0, 255, 0, 255):
+        if pixels[x, y] == color:
             shifted_x = x - width / 2
             normalized_x_gl = '%.5f' % (shifted_x / (width / 2))
             shifted_y = height / 2 - y
